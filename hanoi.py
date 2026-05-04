b8a1f4d147ba092a43f502d8857bf620350dded1
enuel20 <emmaonana18@gmail.com>
2026-05-04 05:26:20 +0100

    Refactor Tower of Hanoi helper layout

--- hanoi.py
+++ hanoi.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python3
 
 
-def print_rods(rods):
-    print(f"A: {rods['A']}")
-    print(f"B: {rods['B']}")
-    print(f"C: {rods['C']}")
-    print("-" * 25)
-
-
-def move_disk(rods, source, target):
-    disk = rods[source].pop()
-    rods[target].append(disk)
-
-    print(f"Move disk {disk} from {source} to {target}")
-    print_rods(rods)
-
-
 def hanoi(n, source, target, auxiliary, rods):
     # If there are no disks to move, stop the recursion.
     if n == 0:
@@ -30,6 +15,21 @@ def hanoi(n, source, target, auxiliary, rods):
     # second recurive ection
     # Move the n-1 disks from auxiliary to target using source as temporary storage.
     hanoi(n - 1, auxiliary, target, source, rods)
+
+
+def move_disk(rods, source, target):
+    disk = rods[source].pop()
+    rods[target].append(disk)
+
+    print(f"Move disk {disk} from {source} to {target}")
+    print_rods(rods)
+
+
+def print_rods(rods):
+    print(f"A: {rods['A']}")
+    print(f"B: {rods['B']}")
+    print(f"C: {rods['C']}")
+    print("-" * 25)
 
 
 def main():
